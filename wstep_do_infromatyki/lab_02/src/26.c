284cd9ee59003c4dc43ffe25ebf773e753a2d81f
Nignik <maksymilian.olbrys@gmail.com>
2025-11-08 15:11:09 +0100

    .

--- wstep_do_infromatyki/lab_02/src/26.c
+++ wstep_do_infromatyki/lab_02/src/26.c
@@ -8,6 +8,7 @@
 
 static int h = 0;
 
+// Funkcje porownawcze dla poszegolnych podpunktow
 bool ret_false(int i, int j) { return false; }
 bool task_a(int i, int j) { return true; }
 bool task_b(int i, int j) { return j <= i; }
@@ -16,24 +17,31 @@ bool task_d(int i, int j) { return j >= h-i-1; }
 bool task_e_1(int i, int j) { return j >= i; }
 bool task_e_2(int i, int j) { return j < h-i-1; }
 
-void print_shape(char task, bool (*compare_1)(int, int), bool (*compare_2)(int, int)) {
-  printf("%c)\n", task);
-  for (int i = 0; i < h; i++) {
-    for (int j = 0; j < h; j++)
-      printf("%c", compare_1(i, j) ? '*' : ' ');
-    for (int j = 0; j < h; j++)
-      printf("%c", compare_2(i, j) ? '*' : ' ');
-    printf("\n");
-  }
+// Funkcja ktora jako argumenty przyjmuje funkcje porownawcze, w celu wypisania wiersza
+void print_row(int i, bool (*compare_1)(int, int), bool (*compare_2)(int, int)) {
+  for (int j = 0; j < h; j++)
+    printf("%c", compare_1(i, j) ? '*' : ' ');
+  for (int j = 0; j < h; j++)
+    printf("%c", compare_2(i, j) ? '*' : ' ');
 }
 
 int main() {
   printf("Prosze podac h:\n");
   scanf("%d", &h);
 
-  print_shape('a', task_a, ret_false);
-  print_shape('b', task_b, ret_false);
-  print_shape('c', task_c, ret_false);
-  print_shape('d', task_d, ret_false);
-  print_shape('e', task_e_1, task_e_2);
+  // wypisanie oznaczen podpunktow w odpowiednich odstepach
+  const char* tasks[5] = {"a)", "b)", "c)", "d)", "e)"};
+  for (int j = 0; j < h*10 - 5*2; j++) {
+    printf("%s", j % (2*h - 1) == 0 ? tasks[j/(2*h-1)] : " ");
+  }
+  printf("\n\n");
+
+  for (int i = 0; i < h; i++) {
+    print_row(i, task_a, ret_false);
+    print_row(i, task_b, ret_false);
+    print_row(i, task_c, ret_false);
+    print_row(i, task_d, ret_false);
+    print_row(i, task_e_1, task_e_2);
+    printf("%c", '\n');
+  }
 }
