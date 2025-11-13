996a37d83416c1f3c48288af1bef363043a2bece
Nignik <maksymilian.olbrys@gmail.com>
2025-11-13 21:32:57 +0100

    wdi: lab 3

--- wstep_do_infromatyki/lab_03/src/33.c
+++ wstep_do_infromatyki/lab_03/src/33.c
@@ -5,27 +5,32 @@
 
 #include <stdio.h>
 #include <stdbool.h>
-
-double a, b, c;
-
-void f(int x) {
-  return a*x*x + b*x + c;
-}
-
-void df(int x) {
-  return 2*a*x + b;
-}
+#include <math.h>
 
 int main() {
-  printf("Prosze podac a: ");
-  scanf("%d", &a);
-  printf("Prosze podac b: ");
-  scanf("%d", &b);
-  printf("Prosze podac c: ");
-  scanf("%d", &c);
-
+  double a, b, c;
   char cont = 'T';
   while (cont == 'T') {
-    
+    printf("Prosze podac a: ");
+    scanf("%lf", &a);
+    printf("\nProsze podac b: ");
+    scanf("%lf", &b);
+    printf("\nProsze podac c: ");
+    scanf("%lf", &c);
+
+    double delta = b*b - 4*a*c;
+    if (delta < 0)
+      printf("\nNie ma pierwiastkow\n");
+    else if (delta == 0)
+      printf("\nJeden pierwiastek: x1 = %lf\n", -b/(2*a));
+    else {
+      delta = sqrt(delta);
+      printf("\nDwa pierwiastki: x1 = %lf, x2 = %lf\n", (-b+delta)/(2*a), (-b-delta)/(2*a));
+    }
+
+    printf("Kontynuowac? ('T', 'N')\n");
+    fflush(stdin);
+    scanf("%c", &cont);
+    fflush(stdin);
   }
 }
--- wstep_do_infromatyki/lab_03/src/34.c
+++ wstep_do_infromatyki/lab_03/src/34.c
@@ -12,11 +12,9 @@ int tabZ[N] = {625,640,631,621,7,773,722,735,741,779,772,780,778,850,77,856,855,
 
 void print() {
   int max_space = 0;
-  for (int i = 0; i < N; i++)
-    max_space = fmax(max_space, snprintf(NULL, 0, "%d", tabZ[i]));
 
   for (int i = 0; i < N; i++)
-    printf("%*d%c", max_space, tabZ[i], ((i+1) % 10 == 0) ? '\n' : ' ');
+    printf("%*d %c", 3, tabZ[i], ((i+1) % 10 == 0) ? '\n' : ' ');
 }
 
 
