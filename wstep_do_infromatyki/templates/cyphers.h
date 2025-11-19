f122e9b9d7358cc5b79d0e8106301d8b23d6c784
Nignik <maksymilian.olbrys@gmail.com>
2025-11-19 18:31:42 +0100

    'idk'

--- wstep_do_infromatyki/templates/cyphers.h
+++ wstep_do_infromatyki/templates/cyphers.h
@@ -7,12 +7,7 @@
 void cesar_cypher(char str[], int k) {
   int sz = strlen(str);
   for (int i = 0; i < sz; i++) {
-    if (!isalpha(str[i]))
-      continue;
-
-    if (isupper(str[i]))
-      str[i] = (str[i] + k - 'A') % 27 + 'A';
-    else
-      str[i] = (str[i] + k - 'a') % 27 + 'a';
+    if (isupper(str[i])) str[i] = (str[i] + k - 'A') % 27 + 'A';
+    if (islower(str[i])) str[i] = (str[i] + k - 'a') % 27 + 'a';
   }
 }
